99aff6cc192580ee8df538b05a67eebf13204c84
GodWindows <kanlinsougodwin@gmail.com>
2024-05-18 18:16:43 +0200

    Installed a method useful for swiping texts, (one after another) on the screen.

--- app/src/main/java/com/godwindows/pianochords/MainActivity.java
+++ app/src/main/java/com/godwindows/pianochords/MainActivity.java
@@ -1,6 +1,9 @@
 package com.godwindows.pianochords;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView chordText;
+    private  boolean[] running = {true};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        chordText =  findViewById(R.id.chordText);
+        swipeChords(5000);
+        Handler myHandler = new Handler(Looper.getMainLooper());
+        myHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeChords(5000);
+            }
         });
+    }
+
+    protected void swipeChords(int timeGap){
+        chordText.setText("Chord n° 0");
+        Handler myHandler = new Handler(Looper.getMainLooper());
+        final int[] i={1};
+        final boolean[] isAlreadyRunning = {false};
+        myHandler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        if (i[0] < 5) {
+                            chordText.setText("Chord n° " + i[0]);
+                            i[0]++;
+                            if(running[0]){
+                                myHandler.postDelayed(this, timeGap);
+                            }
+                        }
+                    }
+                },
+                timeGap
+        );
     }
 }
