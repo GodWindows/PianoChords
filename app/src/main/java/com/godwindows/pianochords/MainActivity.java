16e5408b7ad494ec1e5942ebd0abfb72f37a507b
GodWindows <kanlinsougodwin@gmail.com>
2024-05-18 20:54:10 +0200

    Added two buttons (start and reset) to manage the text swiping

--- app/src/main/java/com/godwindows/pianochords/MainActivity.java
+++ app/src/main/java/com/godwindows/pianochords/MainActivity.java
@@ -3,7 +3,11 @@ package com.godwindows.pianochords;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,37 +17,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     private TextView chordText;
+    private Button resetButton;
+    private Button startButton;
     private  boolean[] running = {true};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chordText =  findViewById(R.id.chordText);
-        swipeChords(5000);
-        Handler myHandler = new Handler(Looper.getMainLooper());
-        myHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                swipeChords(5000);
-            }
-        });
+        resetButton = (Button) findViewById(R.id.resetButton);
+        startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        running[0] = true;
+                        startButton.setActivated(false);
+                        resetButton.setActivated(true);
+                        swipeChords(2000);
+                        Toast.makeText(MainActivity.this, "Début", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        resetButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetButton.setActivated(false);
+                        startButton.setActivated(true);
+                        running[0] = false;
+                        Toast.makeText(MainActivity.this, "Stop", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
     }
 
     protected void swipeChords(int timeGap){
         chordText.setText("Chord n° 0");
         Handler myHandler = new Handler(Looper.getMainLooper());
         final int[] i={1};
-        final boolean[] isAlreadyRunning = {false};
         myHandler.postDelayed(
                 new Runnable() {
                     @Override
                     public void run() {
-                        if (i[0] < 5) {
+                        if (i[0] < 20 && running[0]) {
                             chordText.setText("Chord n° " + i[0]);
                             i[0]++;
-                            if(running[0]){
                                 myHandler.postDelayed(this, timeGap);
-                            }
                         }
                     }
                 },
